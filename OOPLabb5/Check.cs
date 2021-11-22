35f66aa3bc2720b36019902fe315cfd279722baf
Jenny Kallberg <jenka00@hotmail.com>
2021-11-22 10:58:06 +0100

    Add project files.

---
+++ OOPLabb5/Check.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLabb5
+{
+    class Check<Unknown>
+    {
+        public virtual bool toCheck(Unknown number1, Unknown number2)
+        {
+            return number1.Equals(number2);
+        }
+    }
+
+    class Child1<Unknown> : Check<Unknown>
+    {
+        public override bool toCheck(Unknown number1, Unknown number2)
+        {
+            return number1.Equals(number2);
+        }
+    }
+
+    class Child2<Unknown> : Check<Unknown>
+    {
+        public override bool toCheck(Unknown string1, Unknown string2)
+        {
+            return string1.Equals(string2);
+        }
+    }
+
+
+}
---
+++ OOPLabb5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace OOPLabb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child1<int> test1 = new Child1<int>();
+
+            bool result = test1.toCheck(10, 15);
+
+            if (result)
+
+            {
+                Console.WriteLine("Is equal.");
+            }
+
+            else
+            {
+                Console.WriteLine("Is not equal.");
+            }
+
+            Child1<int> test2 = new Child1<int>();
+
+            result = test2.toCheck(10, 10);
+
+            if (result)
+
+            {
+                Console.WriteLine("Is equal.");
+            }
+
+            else
+            {
+                Console.WriteLine("Is not equal");
+            }
+
+            Child2<string> test3 = new Child2<string>();
+
+            result = test3.toCheck("Rätt svar", "Fel svar");
+
+            if (result)
+
+            {
+                Console.WriteLine("Is equal.");
+            }
+
+            else
+            {
+                Console.WriteLine("Is not equal.");
+            }
+
+            Child2<string> test4 = new Child2<string>();
+
+            result = test4.toCheck("Rätt svar", "Rätt svar");
+
+            if (result)
+
+            {
+                Console.WriteLine("Is equal.");
+            }
+
+            else
+            {
+                Console.WriteLine("Is not equal.");
+            }
+        }
+    }
+}
